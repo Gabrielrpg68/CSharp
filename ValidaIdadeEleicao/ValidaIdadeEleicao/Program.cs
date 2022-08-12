405c3421e17a4192d57cedf547f464fd8e471fc6
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-08-12 11:27:16 -0300

    Validando idade para votar

---
+++ ValidaIdadeEleicao/ValidaIdadeEleicao/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nacionalidade: ");
+bool brasileiro = Convert.ToBoolean(Console.ReadLine().ToLower());
+
+if ( idade > 15 && brasileiro == true)
+{
+    Console.WriteLine("Esta apta a votar");
+}
+else
+{
+    Console.WriteLine("Não pode voltar");
+}
