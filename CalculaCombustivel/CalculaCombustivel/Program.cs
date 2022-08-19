a0ce1b24afc3b6e17481402fc4562187e5e7bc3f
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-08-19 11:04:48 -0300

    Add files via upload
    Exercício para calcular qual combustível vale mais a pena.

---
+++ CalculaCombustivel/CalculaCombustivel/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Double gas;
+Double eta;
+
+Console.WriteLine("Valor da Gasolina: ");
+gas = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Valor do Etanol: ");
+eta = Convert.ToDouble(Console.ReadLine());
+
+double valorMaximo = gas - gas * 0.3;
+
+if (valorMaximo < eta)
+{
+    Console.WriteLine("Coloca Gasolina");
+}
+else
+{
+    Console.WriteLine("Coloca Etanol");
+}
+
