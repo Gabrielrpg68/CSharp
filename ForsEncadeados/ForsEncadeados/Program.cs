4d49151e5453d353b72d4b77c981bbe1d4301ac0
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-09-01 00:57:34 -0300

    Exercicio para fazer a tabela de numero com for

---
+++ ForsEncadeados/ForsEncadeados/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ForsEncadeados
+{   
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            int r;
+            for (int i = 1; i <= 4; i++)
+            {
+                String[] numeros = new String[i];
+                for (int index = 0; index < i; index++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        r = i * j;
+                        numeros[j-1] = Convert.ToString(r);
+
+                    }
+                    
+                }
+                Console.WriteLine(String.Join(" ", numeros));
+
+            }
+        }   
+    }   
+}
