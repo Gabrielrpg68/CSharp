ce549ad9d318132fcf209e23dc8d330e6744368e
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-09-01 00:59:43 -0300

    Somar os numeros de 1 a 100, exeto multiplos de 3

---
+++ SomaSemMult3/SomaSemMult3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SomaSemMult3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeroInicial = 1;
+            var numeroFinal = 100;
+            var soma = 0;
+
+            for (int numeroAtual = numeroInicial; numeroAtual <= numeroFinal; numeroAtual++)
+            {
+                var ehMultiploDeTres = numeroAtual % 3 == 0;
+                if (ehMultiploDeTres)
+                    continue;
+
+                soma += numeroAtual;
+            }
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
