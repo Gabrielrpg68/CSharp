541f431f20c4efa99651d387727c42d7c9111c7d
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-09-01 00:56:34 -0300

    Adicionado Exercicio de Fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sequencia de Fibonacci");
+
+                int num1 = 0, num2 = 0, aux;
+
+                while(num2< 100)
+                {   
+                    if(num2 < 1)
+                    {
+                        num2 += 1;
+                        Console.WriteLine("{0}", num2);
+                }
+                    else {
+                        aux = num1;
+                        num1 = num2;
+                        num2 = num1 + aux;
+                        Console.WriteLine("{0}", num2);
+                    }
+                    
+
+                }
+
+
+        }
+
+        
+    }
+}
