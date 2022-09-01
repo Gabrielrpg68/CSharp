3c3a5856be036e76f2a764274f3674a80231ac15
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-09-01 00:56:04 -0300

    Adicionado Exercicio de Fatoriais

---
+++ Fatorial/Fatorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i, numero, fatorial;
+            Console.WriteLine("Informe o número");
+            numero = double.Parse(Console.ReadLine());
+
+            fatorial = numero;
+            for (i = numero - 1; i >= 1; i--)
+            {
+
+                fatorial = fatorial * i;
+
+                Console.WriteLine("i= " + i);
+                Console.WriteLine("fatorial= " + fatorial);
+                Console.WriteLine("  ");
+            }
+            Console.WriteLine("\nFatorial de " + numero + " é " + fatorial);
+            Console.ReadLine();
+        }
+    }
+}
