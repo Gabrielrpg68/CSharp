d8c175f5ac6accf4e0b7aceaaea8118df011cb1d
Gabriel Jesus Dantas <80042075+Gabrielrpg68@users.noreply.github.com>
2022-09-01 00:58:52 -0300

    Exercicio de Somar todos os numeros de 1 a 1000

---
+++ Soma1000/Soma1000/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Soma1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                soma += i;
+            }
+
+            Console.WriteLine("Soma: " + soma);
+        }
+    }
+}
